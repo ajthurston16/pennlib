e0908e634da0e797de9abaf85d95fe249d050a00
AllieRays <allie@zivtech.com>
2016-04-13 13:55:07 -0700

    Modifying taxnonmy vocabulary to be plural to match Drupal coding standards. Adding in more taxonomy terms yaml files for programatically create taxonomy terms for deployment.

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -1,3 +1,3 @@
 <?php
-$settings['hash_salt'] = 'Nltkxadz65F67_VGJxY38Ruw6W2xuYtDE96Lj-UsNnHWb9NJmGIXl2YqWGaQLmSRNkwlRkLf-g';
+$settings['hash_salt'] = '62pYCQLlAuNwjufMKD77gOMfjMtfqdu4ezc9dHS42';
 $config_directories['sync'] = __DIR__ . '/config_m1XsGrr1mSLL/sync';
