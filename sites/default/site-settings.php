1cdfafcffcb7d41ecc09a0bab2b3622b4fc1678e
AllieRays <allie@zivtech.com>
2016-04-26 08:09:41 -0700

    Removing config hash.

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -1,3 +1,3 @@
 <?php
 $settings['hash_salt'] = 'Nltkxadz65F67_VGJxY38Ruw6W2xuYtDE96Lj-UsNnHWb9NJmGIXl2YqWGaQLmSRNkwlRkLf-g';
-$config_directories['sync'] = __DIR__ . '/config_m1XsGrr1mSLL/sync';
+$config_directories['sync'] = __DIR__ . '/config/sync';
