0b5d18c13ccb5f0c7d15bf777dca86cff35c98f0
Ivan Goldsmith <ivangold@upenn.edu>
2016-06-30 08:04:51 -0700

    Add patches to Paragraphs module.
    https://www.drupal.org/node/2716945

--- modules/contrib/paragraphs/src/ParagraphStorageSchema.php
+++ modules/contrib/paragraphs/src/ParagraphStorageSchema.php
@@ -19,19 +19,6 @@ class ParagraphStorageSchema extends SqlContentEntityStorageSchema {
   /**
    * {@inheritdoc}
    */
-  protected function getEntitySchema(ContentEntityTypeInterface $entity_type, $reset = FALSE) {
-    $schema = parent::getEntitySchema($entity_type, $reset);
-
-    $schema['paragraphs_item_field_data']['indexes'] += array(
-      'paragraphs__parent_fields' => array('parent_type', 'parent_id', 'parent_field_name'),
-    );
-
-    return $schema;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   protected function getSharedTableFieldSchema(FieldStorageDefinitionInterface $storage_definition, $table_name, array $column_mapping) {
     // Setting the initial value to 1 when we add a 'status' field.
     // @todo this is a workaround for https://www.drupal.org/node/2346019
