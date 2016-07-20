67159b21990b7ca12552bad73544bc4dd1c08d9b
Ivan Goldsmith <ivangold@upenn.edu>
2016-07-20 06:39:56 -0700

    Cleanup scripts.js (jshint warnings & whitespace)

--- themes/pennlib/js/scripts.js
+++ themes/pennlib/js/scripts.js
@@ -14,10 +14,7 @@
 
   'use strict';
 
-  //
   //Use the sample behavior pattern below
-
-
   Drupal.behaviors.pennlib = {
     attach: function (context, settings) {
       context = context || document;
@@ -38,7 +35,9 @@
             .removeClass('focus');
         });
 
-      // Build a simple responsive hamburger menu
+      /**
+       * Build a simple responsive hamburger menu
+       **/
       $.fn.clickToggle = function (func1, func2) {
         var funcs = [func1, func2];
         this.data('toggleclicked', 0);
@@ -65,48 +64,49 @@
 
       /* to generate the dropdown from the view button when it is clicked - so adding a class 'active' to it
       The class 'active' will take care of the dropdown visibility in CSS*/
-      $('#toggle-view-mode .label').click(function(){
+      $('#toggle-view-mode .label').click(function() {
           $('#toggle-view-mode').toggleClass('active');
       });
 
       $(document).ready(function() {
-        $(document).click(function(event){
-          if(event.target.id!="" && $('#toggle-view-mode').hasClass('active')){
-            $('#toggle-view-mode').removeClass('active'); 
+        $(document).click(function(event) {
+          if (event.target.id!=="" && $('#toggle-view-mode').hasClass('active')) {
+            $('#toggle-view-mode').removeClass('active');
           }
         });
-        if((getCookie('briefview')==='null' && getCookie('normalview')==='null')|| (getCookie('briefview')==='null' && getCookie('normalview')==='true')){
+        if ((getCookie('briefview')==='null' && getCookie('normalview')==='null') ||
+            (getCookie('briefview')==='null' && getCookie('normalview')==='true')) {
           // should evaluate to 'null'
-          console.log('Brief view: '+ getCookie('briefview'));
+          //console.log('Brief view: '+ getCookie('briefview'));
           // should evaluate to 'null'
-          console.log('Normal view: '+ getCookie('normalview'));
+          //console.log('Normal view: '+ getCookie('normalview'));
           if(!($('.block-views-blockstaff-search-block-1').hasClass('normalview'))){
            $('.block-views-blockstaff-search-block-1').addClass('normalview');
           }
         }
-        if(getCookie('briefview')==='true'){
-          if(!($('#view-mode-brief').hasClass('checked'))){
+        if (getCookie('briefview')==='true') {
+          if (!($('#view-mode-brief').hasClass('checked'))) {
             $('#view-mode-brief').addClass('checked');
-          } 
+          }
           $('#view-mode-normal').removeClass('checked');
-          if($('.block-views-blockstaff-search-block-1').hasClass('normalview')){
+          if ($('.block-views-blockstaff-search-block-1').hasClass('normalview')) {
             $('.block-views-blockstaff-search-block-1').removeClass('normalview');
             $('.block-views-blockstaff-search-block-1').addClass('briefview');
           }
-          else{
+          else {
             $('.block-views-blockstaff-search-block-1').addClass('briefview');
           }
         }
-        else{
-          if(!($('#view-mode-normal').hasClass('checked'))){
+        else {
+          if (!($('#view-mode-normal').hasClass('checked'))) {
             $('#view-mode-normal').addClass('checked');
           }
           $('#view-mode-brief').removeClass('checked');
-          if($('.block-views-blockstaff-search-block-1').hasClass('briefview')){
+          if ($('.block-views-blockstaff-search-block-1').hasClass('briefview')) {
             $('.block-views-blockstaff-search-block-1').removeClass('briefview');
             $('.block-views-blockstaff-search-block-1').addClass('normalview');
           }
-          else{
+          else {
             $('.block-view-views-blockstaff-search-block1').addClass('normalview');
           }
         }
@@ -114,20 +114,20 @@
 
       /* When the mode is brief, we add a class 'checked' to it so that it has a checkbox, remove that of the normal mode by removing the class
       If the staff search block has a normal view, change it to brief */
-      $('#view-mode-brief').click(function(){
-        if(!($(this).hasClass('checked'))){
+      $('#view-mode-brief').click(function() {
+        if (!($(this).hasClass('checked'))) {
           $(this).addClass('checked');
         }
         $('#view-mode-normal').removeClass('checked');
-        if($('.block-views-blockstaff-search-block-1').hasClass('normalview')){
+        if ($('.block-views-blockstaff-search-block-1').hasClass('normalview')) {
           $('.block-views-blockstaff-search-block-1').removeClass('normalview');
           $('.block-views-blockstaff-search-block-1').addClass('briefview');
         }
-        else{
+        else {
           $('.block-views-blockstaff-search-block-1').addClass('briefview');
         }
         
-        if(getCookie('briefview')==='null' || getCookie('briefview')==='true'){
+        if (getCookie('briefview')==='null' || getCookie('briefview')==='true') {
           //$.cookie('normalview',null,{path: '/'});
           //$.cookie('briefview','true',{path: '/'});
           document.cookie = 'normalview=null;path=/';
@@ -142,20 +142,20 @@
 
       /* When the mode is normal, we add a class 'checked' to it so that it has a checkbox, remove that of the brief mode by removing the class
       If the staff search block has a brief view, change it to normal */
-      $('#view-mode-normal').click(function(){
-        if(!($(this).hasClass('checked'))){
+      $('#view-mode-normal').click(function() {
+        if (!($(this).hasClass('checked'))) {
           $(this).addClass('checked');
         }
         $('#view-mode-brief').removeClass('checked');
-        if($('.block-views-blockstaff-search-block-1').hasClass('briefview')){
+        if ($('.block-views-blockstaff-search-block-1').hasClass('briefview')) {
           $('.block-views-blockstaff-search-block-1').removeClass('briefview');
           $('.block-views-blockstaff-search-block-1').addClass('normalview');
         }
-        else{
+        else {
           $('.block-view-views-blockstaff-search-block1').addClass('normalview');
         }
         
-        if(getCookie('normalview')==='null' || getCookie('normalview')==='true'){
+        if (getCookie('normalview')==='null' || getCookie('normalview')==='true') {
           document.cookie = 'briefview=null;path=/';
           document.cookie = 'normalview=true;path=/';
           // should evaluate to "false"
@@ -164,7 +164,6 @@
           //console.log('Normal view: '+ getCookie('normalview'));
         }
       });
-
       
       /* 
       *  get the value of cookie corresponding to a particular cookie name (cookieName)
@@ -179,13 +178,13 @@
         var len = kvPairs.length;
         var i = 0;
         //Iterating over all the k-v pairs searching for the searchString, returning its value if it is present , else returning an empty string
-        for(i=0; i<len; i++){
+        for (i=0; i<len; i++) {
           var kvPair = kvPairs[i];
-          var kvPairLen = kvPair.length; 
-          while(kvPair.charAt(0)==' '){
+          var kvPairLen = kvPair.length;
+          while (kvPair.charAt(0)==' ') {
             kvPair = kvPair.substring(1,kvPairLen);
           }
-          if(kvPair.indexOf(searchString) == 0){
+          if (kvPair.indexOf(searchString) === 0){
             return kvPair.substring(searchStringLen, kvPairLen);
           }
         }
