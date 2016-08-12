a9ebe961426864e8ba31c3b7fae26216257a7898
Ivan Goldsmith <ivangold@upenn.edu>
2016-08-12 08:59:11 -0700

    Refactor JS with contexts.

--- themes/pennlib/js/scripts.js
+++ themes/pennlib/js/scripts.js
@@ -15,7 +15,7 @@
   'use strict';
 
   //Use the sample behavior pattern below
-  Drupal.behaviors.pennlib = {
+  Drupal.behaviors.pennlibSearch = {
     attach: function (context, settings) {
       context = context || document;
       settings = settings || Drupal.settings;
@@ -34,10 +34,17 @@
             .closest('.block-search')
             .removeClass('focus');
         });
+    } //end attach
+  }; //end Drupal.behaviors.pennlib
 
-      /**
-       * Build a simple responsive hamburger menu
-       **/
+  /**
+   * Build a simple responsive hamburger menu
+   **/
+  Drupal.behaviors.pennlibHamburger = {
+    attach: function (context, settings) {
+      context = context || document;
+      settings = settings || Drupal.settings;
+
       $.fn.clickToggle = function (func1, func2) {
         var funcs = [func1, func2];
         this.data('toggleclicked', 0);
@@ -61,33 +68,44 @@
         $(this).removeClass('on');
         $mobileMenu.removeClass('show hburgermenu');
       });
+            } //end attach
+          }; //end Drupal.behaviors.pennlib
 
-      /**
-       * View mode toggle for Staff Search (/library-info/staff). Uses cookies to persist on subsequent page loads.
-       **/
+  /**
+   * View mode toggle for Staff Search (/library-info/staff). Uses cookies to persist on subsequent page loads.
+   **/
+  Drupal.behaviors.pennlibViewmode = {
+    attach: function (context, settings) {
+      context = context || document;
+      settings = settings || Drupal.settings;
 
-      // Toggle view mode dropdown when it is clicked.
-      $('#toggle-view-mode .label').click(function() {
-          $('#toggle-view-mode').toggleClass('active');
-      });
       var $staffSearchBlock = $('.block-views-blockstaff-search-block-1');
+      var $normal = $('#view-mode-normal');
+      var $brief = $('#view-mode-brief');
+      var $viewModeToggle = $('#toggle-view-mode');
+
+
+      // adding class active for the arrow icon on menu wrapper
+      $('#toggle-view-mode .label', context).click(function() {
+          $viewModeToggle.toggleClass('active');
+      });
 
       // Toggle classes based on the view mode. 'mode' argument should be either 'normalview' or 'briefview'.
       function toggleViewMode(mode) {
         if (mode === 'normalview') {
-          if (!($('#view-mode-normal').hasClass('checked'))) {
-            $('#view-mode-normal').addClass('checked');
+          if (!($normal.hasClass('checked'))) {
+            $normal.addClass('checked');
           }
-          $('#view-mode-brief').removeClass('checked');
+          $brief.removeClass('checked');
           if ($staffSearchBlock.hasClass('briefview')) {
             $staffSearchBlock.removeClass('briefview');
           }
           $staffSearchBlock.addClass('normalview');
         } else if (mode === 'briefview') {
-          if (!($('#view-mode-brief').hasClass('checked'))) {
-            $('#view-mode-brief').addClass('checked');
+          if (!($brief.hasClass('checked'))) {
+            $brief.addClass('checked');
           }
-          $('#view-mode-normal').removeClass('checked');
+          $normal.removeClass('checked');
           if ($staffSearchBlock.hasClass('normalview')) {
             $staffSearchBlock.removeClass('normalview');
           }
@@ -95,33 +113,14 @@
         }
       } //end toggleViewMode()
 
-      $(document).ready(function() {
-        // Repopulate the library home link if there is a homepage cookie.
-        if (getCookie('hp')) {
-          $('#chooselibrarylink a').attr('href', 'http://www.library.upenn.edu' + getCookie('hp'));
-        }
-
-        // Sitewide Franklin login scripts: Asynchronously load the scripts and execute them in order.
-        // We're loading asynchronously so that when there is a Franklin/DLA outage, it doesn't crash the rest of the page.
-        $("#top-nav-pennkey-library-account").prepend('<span id="helloname" class="helloname"></span><span class="hellocolon">:</span>');
-        $.getScript("/themes/pennlib/js/login/loginStatusModule.js", function() {
-          $.getScript("/themes/pennlib/js/login/login.js", function() {
-            $.getScript("http://dla.library.upenn.edu/dla/franklin/scripts/json2.js", function() {
-              $.getScript("http://dla.library.upenn.edu/dla/franklin/scripts/storage.js", function() {
-                // All scripts have been loaded. Any code that needs to happen after that can go here.
-                console.log("Done loading login scripts!");
-              });
-            });
-          });
-        });
-
+    //  $(document).ready(function() {
         // Staff: Hide dropdown if you click outside of it.
         // For some reason, event.target.id keeps returning undefined values for any clicked element.
         // Checking that id would be a more direct way of doing this.
         $(document).click(function(event) {
           var eventNotInDropdown = !($(event.target).closest('#toggle-view-mode').length);
           if (eventNotInDropdown && $('#toggle-view-mode').hasClass('active')) {
-            $('#toggle-view-mode').removeClass('active');
+            $viewModeToggle.removeClass('active');
           }
         });
 
@@ -129,7 +128,7 @@
         if (getCookie('briefview')==='true') {
           toggleViewMode('briefview');
         } else {
-          toggleViewMode('normalview');
+          toggleViewMode('  ');
         }
 
         // Staff: If on the staff search page, use hidden booleans to show/hide certain fields.
@@ -142,66 +141,97 @@
             $(this).find(".field--name-field-division").addClass("showinbrief");
           }
         });
-      });
+   //   }); //end document.ready
 
       // When a view mode is selected, toggle the classes and update the view mode cookie.
-      $('#view-mode-brief').click(function() {
+      $brief.click(function() {
         toggleViewMode('briefview');
         document.cookie = 'briefview=true;path=/';
         document.cookie = 'normalview=;path=/';
         // Making a selection should hide the entire dropdown menu
-        $('#toggle-view-mode').toggleClass('active');
+        $viewModeToggle.toggleClass('active');
       });
-      $('#view-mode-normal').click(function() {
+      $normal.click(function() {
         toggleViewMode('normalview');
         document.cookie = 'briefview=;path=/';
         document.cookie = 'normalview=true;path=/';
         // Making a selection should hide the entire dropdown menu
-        $('#toggle-view-mode').toggleClass('active');
+        $viewModeToggle.toggleClass('active');
       });
+    } //end attach
+  }; //end Drupal.behaviors.pennlib
 
-     /**
-      * Get the value of cookie corresponding to a particular cookie name (cookieName)
-      * @param cookieName (name of the cookie)
-      **/
-      function getCookie(cookieName){
-        //The string to be searched for in document.cookie's k-v pairs
-        var searchString = cookieName + "=";
-        var searchStringLen = searchString.length;
-        //To generate an array of k-v pairs corresponding to the elements in document.cookie
-        var kvPairs = document.cookie.split(';');
-        var len = kvPairs.length;
-        var i = 0;
-        //Iterating over all the k-v pairs searching for the searchString, returning its value if it is present, else returning an empty string
-        for (i=0; i<len; i++) {
-          var kvPair = kvPairs[i];
-          var kvPairLen = kvPair.length;
-          while (kvPair.charAt(0)==' ') {
-            kvPair = kvPair.substring(1,kvPairLen);
-          }
-          if (kvPair.indexOf(searchString) === 0){
-            return kvPair.substring(searchStringLen, kvPairLen);
-          }
-        }
-        return null;
+  /**
+  * Get the value of cookie corresponding to a particular cookie name (cookieName)
+  * @param cookieName (name of the cookie)
+  **/
+  function getCookie(cookieName){
+    //The string to be searched for in document.cookie's k-v pairs
+    var searchString = cookieName + "=";
+    var searchStringLen = searchString.length;
+    //To generate an array of k-v pairs corresponding to the elements in document.cookie
+    var kvPairs = document.cookie.split(';');
+    var len = kvPairs.length;
+    var i = 0;
+    //Iterating over all the k-v pairs searching for the searchString, returning its value if it is present, else returning an empty string
+    for (i=0; i<len; i++) {
+      var kvPair = kvPairs[i];
+      var kvPairLen = kvPair.length;
+      while (kvPair.charAt(0)==' ') {
+        kvPair = kvPair.substring(1,kvPairLen);
       }
+      if (kvPair.indexOf(searchString) === 0){
+        return kvPair.substring(searchStringLen, kvPairLen);
+      }
+    }
+    return null;
+  }
 
-      /**
-       * Menubar: top-level link stays white when you hover on dropdown panel
-       **/
-      $(".main-nav > .main-nav__item  > ul.main-nav__sub")
+  /**
+   * Menubar: top-level link stays white when you hover on dropdown panel
+   **/
+  Drupal.behaviors.pennlibMenubar = {
+    attach: function (context, settings) {
+      context = context || document;
+      settings = settings || Drupal.settings;
+      $(".main-nav > .main-nav__item  > ul.main-nav__sub", context)
         .on("mouseover", function () {
           $(this).parent('li').addClass('is-hovered');
         })
         .on("mouseleave", function () {
           $(this).parent('li').removeClass('is-hovered');
         });
+    } //end attach
+  }; //end Drupal.behaviors.pennlib
 
-      /**
-       * Top-nav: dropdowns.
-       **/
+  /**
+   * Top-nav: dropdowns, homepage link, and login.
+   **/
+  Drupal.behaviors.pennlibTopnav = {
+    attach: function (context, settings) {
+      context = context || document;
+      settings = settings || Drupal.settings;
+      // Repopulate the library home link if there is a homepage cookie.
+      if (getCookie('hp')) {
+        $('#chooselibrarylink a', context).attr('href', 'http://www.library.upenn.edu' + getCookie('hp'));
+      }
 
-      $('.idbarbutton.clickableArrow').click(function() {
+      // Sitewide Franklin login scripts: Asynchronously load the scripts and execute them in order.
+      // We're loading asynchronously so that when there is a Franklin/DLA outage, it doesn't crash the rest of the page.
+      $("#top-nav-pennkey-library-account", context).prepend('<span id="helloname" class="helloname"></span><span class="hellocolon">:</span>');
+      $.getScript("/themes/pennlib/js/login/loginStatusModule.js", function() {
+        $.getScript("/themes/pennlib/js/login/login.js", function() {
+          $.getScript("http://dla.library.upenn.edu/dla/franklin/scripts/json2.js", function() {
+            $.getScript("http://dla.library.upenn.edu/dla/franklin/scripts/storage.js", function() {
+              // All scripts have been loaded. Any code that needs to happen after that can go here.
+              console.log("Done loading login scripts!");
+            });
+          });
+        });
+      });
+
+      // Dropdowns
+      $('.idbarbutton.clickableArrow', context).click(function() {
         // If the button isn't active, toggle dropdown the menu on, make it active, and make any other active buttons inactive
         if(!$(this).hasClass('active')){
           $(this).next().show(); // toggle the actual menu
@@ -228,32 +258,27 @@
           $('.idbarbutton.clickableArrow').next().hide();
         }
       });
+    } //end attach
+  }; //end Drupal.behaviors.pennlib
 
-
-      /**
-       * Site-wide Search Bar: switches between different searches with radio buttons.
-       **/
-      $('#searchholderlining input[type="radio"]').click(function() {
+  /**
+   * Site-wide Search Bar: switches between different searches with radio buttons.
+   **/
+  Drupal.behaviors.pennlibSearchbar = {
+    attach: function (context, settings) {
+      context = context || document;
+      settings = settings || Drupal.settings;
+      $('#searchholderlining input[type="radio"]', context).click(function() {
           $('#searchholderlining .searchform').hide();
           $('#'+$(this).val()).show();
       });
 
       // Toggle radio button active class
-      $('#radiobluebuttons .holdradio').click(function() {
+      $('#radiobluebuttons .holdradio', context).click(function() {
         $(this).addClass('active');
         $('#searchholderlining .holdradio').not(this).removeClass('active');
       });
-
     } //end attach
   }; //end Drupal.behaviors.pennlib
-
-  //
-  // Polyfill things where needed
-  //
-  Drupal.behaviors.bearPolyfill = {
-    attach: function (context, settings) {
-
-    }
-  };
 
 })(jQuery, Drupal, window, document);
